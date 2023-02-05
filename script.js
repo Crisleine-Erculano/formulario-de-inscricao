952570e319598d7cbca88b57304bee0a4d621220
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-02-05 18:25:39 -0300

    Criando a lógica de validação dos dados do formulário

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function valida_form (){
+    if(document.getElementById("first").value=="" || document.getElementById('last').value=="" || document.getElementById("email").value=="" || document.getElementById("password").value == ""){
+    alert('Por favor, preencha Todos os campos');
+    
+     }
+    }
